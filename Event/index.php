702edd714b40ff015ee1efba7a83a2526d067ef8
RafliFathan969 <kyzenn40@gmail.com>
2025-11-21 10:01:40 +0700

    membuat modal detail

--- Event/index.php
+++ Event/index.php
@@ -50,10 +50,52 @@
                                         <td><?= $data['nama_event'] ?></td>
                                         <td><?= $data['lokasi'] ?></td>
                                         <td><?= $data['tanggal_event'] ?></td>
-                                    </tr>
-                                    <td>
-                                        
+                                        <td>
+                                        <button class="btn btn-warning btn-sm" data-bs-toggle="modal" data-bs-target="#exampleModal<?= $data['id_event'] ?>"><i class="fa-solid fa-globe"></i></button>
+                                        <a href="" class="btn btn-info btn-sm"><i class="fa-solid fa-folder-plus"></i></a>
+                                        <button class="btn btn-danger btn-sm" data-bs-toggle="modal" data-bs-target="#modalhapus"><i class="fa-solid fa-folder-minus"></i></button>
+
+                                        <div class="modal fade" id="exampleModal<?= $data['id_event'] ?>" tabindex="-1"
+                                                aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                                <div class="modal-dialog">
+                                                    <div class="modal-content">
+                                                        <div class="modal-header">
+                                                            <h1 class="modal-title fs-5" id="exampleModalLabel">Data Detail
+                                                                <?= $data['nama_event'] ?>
+                                                            </h1>
+                                                            <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                                                aria-label="Close"></button>
+                                                        </div>
+                                                        <div class="modal-body">
+                                                            <table class="table">
+                                                                <tbody>
+                                                                    <tr>
+                                                                        <td>Nama event</td>
+                                                                        <th scope="row"><?= $data['nama_event'] ?></th>
+                                                                    </tr>
+                                                                    <tr>
+                                                                        <td>Lokasi</td>
+                                                                        <th scope="row"><?= $data['lokasi'] ?></th>
+                                                                    </tr>
+                                                                    <tr>
+                                                                        <td>Tanggal</td>
+                                                                        <th scope="row"><?= $data['tanggal_event'] ?></th>
+                                                                    </tr>
+                                                                    <tr>
+                                                                        <td>Jumlah Peserta</td>
+                                                                        <th scope="row"><?= $data['jumlah_peserta'] ?></th>
+                                                                    </tr>
+                                                                </tbody>
+                                                            </table>
+                                                        </div>
+                                                        <div class="modal-footer">
+                                                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
                                     </td>
+                                    </tr>
                                 </tbody>
                                 <?php
                             }
